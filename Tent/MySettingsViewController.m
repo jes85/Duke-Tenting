0185b0dfef3e7b10b97f10285eda7028725a65e2
Jeremy Schreck <jes85@duke.edu>
2015-12-28 17:54:35 -0800

    Changing user settings works

--- Tent/MySettingsViewController.m
+++ Tent/MySettingsViewController.m
@@ -13,7 +13,7 @@
 @interface MySettingsViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic) NSArray *settings;
-@property (nonatomic) NSArray *settingValues;
+@property (nonatomic) NSMutableArray *settingValues;
 
 @end
 
@@ -23,31 +23,16 @@
     [super viewDidLoad];
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
-    [self testChangeEmail];
     PFUser *currentUser = [PFUser currentUser];
 
-    self.settings = @[@"Username:", @"Password:", @"Email:", @"Full Name:"];
-    self.settingValues = @[currentUser.username, @"Password", currentUser.email, [currentUser objectForKey:kUserPropertyFullName]];
+    self.settings = @[@"Username", @"Password", @"Email", @"Full Name"];
+    self.settingValues = [[NSMutableArray alloc]initWithArray:@[currentUser.username, @"Change Password", currentUser.email, [currentUser objectForKey:kUserPropertyFullName]]];
     // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
--(void)testChangeEmail{
-    PFQuery *query = [PFQuery queryWithClassName:@"_User"];
-    [query getObjectInBackgroundWithId:[[PFUser currentUser] objectId] block:^(PFObject *object, NSError *error) {
-        if(!error){
-                object[@"email"] = @"test@gmail.com";
-                [object saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                    if(error){
-                        
-                    }
-                }];
-        }
-    }];
 }
 
 #pragma mark - UITableViewDataSource methods
@@ -71,6 +56,146 @@
     
     return cell;
 }
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if([self.settings[indexPath.row] isEqualToString:@"Username"]){
+        [self changeUsername];
+        
+    }else if([self.settings[indexPath.row] isEqualToString:@"Password"]){
+        [self changePassword];
+        
+    }else if([self.settings[indexPath.row] isEqualToString:@"Email"]){
+        [self changeEmail];
+        
+    }else if ([self.settings[indexPath.row] isEqualToString:@"Full Name"]){
+        [self changeFullName];
+    }
+}
+-(void)changeUsername
+{
+    [self showChangeUsernameAlertWithMessage:@"Enter a new username."];
+}
+-(void)showChangeUsernameAlertWithMessage:(NSString *)message
+{
+    UIAlertController *alert = [self changeSettingAlertWithTitle:@"Change Username" message:message placeholder:[[PFUser currentUser] username]];
+    UIAlertAction *changeAction = [UIAlertAction actionWithTitle:@"Change" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        UITextField *textField = alert.textFields.firstObject;
+        PFUser *currentUser = [PFUser currentUser];
+        NSString *oldSettingValue = currentUser.username;
+        NSString *newSettingValue = textField.text;
+        //Update current user on parse
+        currentUser.username = newSettingValue;
+        [currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if(succeeded){
+                self.settingValues[0] = newSettingValue;
+                
+                //Change this to only update desired cell
+                [self.tableView reloadData];
+            }else{
+                if(error.code == 202){
+                    currentUser.username = oldSettingValue;
+                    [self showChangeUsernameAlertWithMessage:[NSString stringWithFormat:@"Username %@ has already been taken. Try again", newSettingValue]];
+                }
+            }
+        }];
+    }];
+    [alert addAction:changeAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+-(void)changePassword
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Change Password" message:@"We will send you an email with a link to change your password." preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+    
+    UIAlertAction *sendAction = [UIAlertAction actionWithTitle:@"Send" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        [PFUser requestPasswordResetForEmailInBackground:[[PFUser currentUser] email] block:^(BOOL succeeded, NSError *error) {
+            if(succeeded){
+                //show success alert
+                UIAlertController *successAlert = [UIAlertController alertControllerWithTitle:@"Success!" message:@"Check your email." preferredStyle:UIAlertControllerStyleAlert];
+                [successAlert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
+                [self presentViewController:successAlert animated:YES completion:nil];
+                
+            }
+        }];
+
+    }];
+    [alert addAction:sendAction];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+    }
+-(void)changeEmail
+{
+    [self showChangeEmailAlertWithMessage:@"Enter a new email."];
+}
+-(void)showChangeEmailAlertWithMessage:(NSString *)message
+{
+    UIAlertController *alert = [self changeSettingAlertWithTitle:@"Change Email" message:message placeholder:[[PFUser currentUser] email]];
+    UIAlertAction *changeAction = [UIAlertAction actionWithTitle:@"Change" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        UITextField *textField = alert.textFields.firstObject;
+        PFUser *currentUser = [PFUser currentUser];
+
+        NSString *oldSettingValue = currentUser.email;
+        NSString *newSettingValue = textField.text;
+        //Update current user on parse
+        currentUser.email = newSettingValue;
+        [currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if(succeeded){
+                //Change this to only update desired cell
+                self.settingValues[2] = newSettingValue;
+                [self.tableView reloadData];
+            }else{
+                if(error.code == 125){
+                    //alert.message = @"Invalid email address. Try again";
+                    currentUser.email = oldSettingValue;
+                    [self showChangeEmailAlertWithMessage:@"Invalid email address. Try again"];
+                }else if (error.code == 203){
+                    //alert.message = [NSString stringWithFormat:@"Email address %@ has already been taken. Try again", newSettingValue];
+                    currentUser.email = oldSettingValue;
+                    [self showChangeEmailAlertWithMessage:[NSString stringWithFormat:@"Email address %@ has already been taken. Try again", newSettingValue]];
+                    
+                }
+            }
+        }];
+    }];
+    [alert addAction:changeAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+-(void)changeFullName
+{
+    UIAlertController *alert = [self changeSettingAlertWithTitle:@"Change Name" message:@"Enter a new name." placeholder:[[PFUser currentUser] objectForKey:kUserPropertyFullName]];
+    UIAlertAction *changeAction = [UIAlertAction actionWithTitle:@"Change" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        UITextField *textField = alert.textFields.firstObject;
+        NSString *newSettingValue = textField.text;
+        //Update current user on parse
+        PFUser *currentUser = [PFUser currentUser];
+        [currentUser setValue:newSettingValue forKey:kUserPropertyFullName];
+        [currentUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if(succeeded){
+                self.settingValues[3] = newSettingValue;
+                //Change this to only update desired cell
+                [self.tableView reloadData];
+            }
+        }];
+    }];
+    [alert addAction:changeAction];
+    [self presentViewController:alert animated:YES completion:nil];
+
+}
+-(UIAlertController *)changeSettingAlertWithTitle:(NSString *)title message:(NSString *)message placeholder:(NSString *)placeholder
+{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.placeholder = placeholder;
+    }];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:cancelAction];
+    return alert;
+}
+
 - (IBAction)logOutButtonPressed:(id)sender {
     [PFUser logOut];
     [self performSegueWithIdentifier:@"closeSettingsSegue" sender:self];
