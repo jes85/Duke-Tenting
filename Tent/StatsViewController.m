842e4a47c37810e9dc26700743a53bbd9df33281
Jeremy Schreck <jes85@duke.edu>
2016-01-10 13:37:14 -0500

    Modified Stats VC

--- Tent/StatsViewController.m
+++ Tent/StatsViewController.m
@@ -12,6 +12,8 @@
 @interface StatsViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *minutesAssignedLabel;
 @property (weak, nonatomic) IBOutlet UILabel *minutesAvailableLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nightsAssignedLabel;
+@property (weak, nonatomic) IBOutlet UILabel *nightsAvailableLabel;
 
 @end
 
@@ -25,16 +27,33 @@
     self.navigationItem.backBarButtonItem = back;
     self.navigationItem.title = self.person.user ? [self.person.user objectForKey:kUserPropertyFullName] : self.person.offlineName;
     
-    NSArray *info = [self calculateMinutesAvailableAndAssigned];
-    NSUInteger minutesAvailable = [info[0] unsignedIntegerValue];
-    NSUInteger minutesAssigned = [info[1] unsignedIntegerValue];
+    NSArray *dayInfo = [self calculateMinutesAvailableAndAssigned];
+    NSUInteger dayTotalMinutesAvailable = [dayInfo[0] unsignedIntegerValue];
+    NSUInteger dayTotalMinutesAssigned = [dayInfo[1] unsignedIntegerValue];
+    
+    /*
+    NSUInteger dayHoursAvailable = dayTotalMinutesAvailable / 60;
+    NSUInteger dayMinutesAvailable = dayTotalMinutesAvailable % 60;
+    
+    
+    NSUInteger dayHoursAssigned = dayTotalMinutesAssigned / 60;
+    NSUInteger dayMinutesAssigned = dayTotalMinutesAssigned % 60;
+     */
+    
+    NSArray *nightStats = [self calculateNightsAvailableAndAssigned];
+    NSUInteger nightsAvailable = [nightStats[0] unsignedIntegerValue];
+    NSUInteger nightsAssigned = [nightStats[1] unsignedIntegerValue];
 
-    self.minutesAvailableLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)minutesAvailable];
-    self.minutesAssignedLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)minutesAssigned];
+    
+
+    self.minutesAvailableLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)dayTotalMinutesAvailable];
+    self.minutesAssignedLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)dayTotalMinutesAssigned];
+    self.nightsAvailableLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)nightsAvailable];
+    self.nightsAssignedLabel.text = [NSString stringWithFormat:@"%lu", (unsigned long)nightsAssigned];
     
     
     // Testing
-    [self calculateTotalNightAndDayIntervalsAvailableAndAssigned];
+    //[self calculateTotalNightAndDayIntervalsAvailableAndAssigned];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -42,7 +61,59 @@
     // Dispose of any resources that can be recreated.
 }
 
-
+-(NSArray *)calculateNightsAvailableAndAssigned
+{
+    NSUInteger nightsAvailable = 0;
+    NSUInteger nightsAssigned = 0
+    
+    ;
+    for(int i = 0; i <self.person.assignmentsArray.count; i++){
+        
+        NSUInteger intervalStatus = [self.person.assignmentsArray[i] unsignedIntegerValue];
+        if(intervalStatus != 0){
+            Interval *interval = self.schedule.intervalDataByOverallRow[i];
+            if(!interval.night) continue;
+            if(intervalStatus == 2){
+                nightsAssigned += 1;
+                nightsAvailable += 1;
+            }
+            else if(intervalStatus == 1){
+                nightsAvailable += 1;
+                
+            }
+            
+        }
+    }
+    return @[[NSNumber numberWithUnsignedInteger:nightsAvailable], [NSNumber numberWithUnsignedInteger:nightsAssigned]];
+}
+-(NSArray *)calculateDayMinutesAvailableAndAssigned
+{
+    NSUInteger minutesAvailable = 0;
+    NSUInteger minutesAssigned = 0
+    
+    ;
+    for(int i = 0; i <self.person.assignmentsArray.count; i++){
+        
+        NSUInteger intervalStatus = [self.person.assignmentsArray[i] unsignedIntegerValue];
+        if(intervalStatus != 0){
+            Interval *interval = self.schedule.intervalDataByOverallRow[i];
+            if(interval.night) continue;
+            NSUInteger seconds = [interval.endDate timeIntervalSinceDate:interval.startDate];
+            NSUInteger minutes = seconds/60;
+            if(intervalStatus == 2){
+                minutesAssigned += minutes;
+                minutesAvailable += minutes;
+            }
+            else if(intervalStatus == 1){
+                minutesAvailable += minutes;
+                
+            }
+            
+        }
+    }
+    
+    return @[[NSNumber numberWithUnsignedInteger:minutesAvailable], [NSNumber numberWithUnsignedInteger:minutesAssigned]];
+}
 -(NSArray *)calculateMinutesAvailableAndAssigned
 {
     NSUInteger minutesAvailable = 0;
